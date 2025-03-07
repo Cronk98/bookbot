a1e14421d935f45db54732a61f5c27ab6e3e06df
Cronk98 <andrewcronk98@gmail.com>
2025-03-07 18:32:39 -0500

    Updated main.py to only display word count

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
+def get_word_count(text):
+    word_count = len(text.split())
+    print(f"{word_count} words found in the document")
+
 def main():
     file_path = "./books/frankenstein.txt"
     book_text = get_book_text(file_path)
-    print(book_text)
+    get_word_count(book_text)
 
 main()

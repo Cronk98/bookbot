90761dead9febd236fad0c5b747adad9dd539720
Cronk98 <andrewcronk98@gmail.com>
2025-03-07 19:08:56 -0500

    Added get_char_count func to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count
+from stats import get_char_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,5 +10,6 @@ def main():
     file_path = "./books/frankenstein.txt"
     book_text = get_book_text(file_path)
     get_word_count(book_text)
+    get_char_count(book_text)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def get_word_count(text):
     word_count = len(text.split())
     print(f"{word_count} words found in the document")
+
+def get_char_count(text):
+    char_dict = {}
+    for word in text.lower():
+        for char in word:
+            if char not in char_dict:
+                char_dict[char] = 1
+            else:
+                char_dict[char] += 1
+    
+    for key, value in char_dict.items():
+        print(f"'{key}': {value}")

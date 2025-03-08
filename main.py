4f369ff39994fc01f2def2a79f9358f608fb6b06
Cronk98 <andrewcronk98@gmail.com>
2025-03-08 11:59:27 -0500

    Added sort_dict func and structured report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_word_count
 from stats import get_char_count
+from stats import sort_dict
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,7 +10,24 @@ def get_book_text(path_to_file):
 def main():
     file_path = "./books/frankenstein.txt"
     book_text = get_book_text(file_path)
-    get_word_count(book_text)
-    get_char_count(book_text)
+    word_count = get_word_count(book_text)
+
+    print("============ BOOKBOT ============\n"
+          "Analyzing book found at books/frankenstein.txt...\n"
+          "----------- Word Count ----------")
+    
+    print(f"Found {word_count} total words")
+
+    print("--------- Character Count -------")
+
+    char_dict = get_char_count(book_text)
+    sorted_dict = sort_dict(char_dict)
+    for pair in sorted_dict:
+        if pair["char"].isalpha() == True:
+            print(f"{pair["char"]}: {pair["count"]}")
+
+    print("============= END ===============")
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def get_word_count(text):
     word_count = len(text.split())
-    print(f"{word_count} words found in the document")
+    return word_count
 
 def get_char_count(text):
     char_dict = {}
@@ -10,6 +10,16 @@ def get_char_count(text):
                 char_dict[char] = 1
             else:
                 char_dict[char] += 1
+    return char_dict
     
     for key, value in char_dict.items():
         print(f"'{key}': {value}")
+
+def sort_dict(dict):
+    dict_list = []
+    for key, value in dict.items():
+        pair = {"char": key, "count": value}
+        dict_list.append(pair)
+    new_list = sorted(dict_list, key=lambda d: d["count"], reverse=True)
+    return new_list
+    
